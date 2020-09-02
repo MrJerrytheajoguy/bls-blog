8a9f40a47ae1e48aff99522e53d2f6079859c21d
Sunny Cletus <scletus@gmail.com>
2020-09-02 12:16:59 +0100

    image upload fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     FIREBASE_AUTH_DOMAIN: "betterlifesavings-30f89.firebaseapp.com",
     FIREBASE_DATABASE_URL: "https://betterlifesavings-30f89.firebaseio.com",
     FIREBASE_PROJECT_ID: "betterlifesavings-30f89",
-    FIREBASE_STORAGE_BUCKET: "betterlifesavings-dev.appspot.com",
+    FIREBASE_STORAGE_BUCKET: "betterlifesavings-30f89.appspot.com",
     FIREBASE_MESSAGING_SENDER_ID: "305956581029",
     FIREBASE_APP_ID: "1:305956581029:web:55a218180e200b0350274c",
     FIREBASE_MEASUREMENT_ID: "G-JR4SJ57715",
